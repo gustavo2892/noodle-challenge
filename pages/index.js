9c16e9849bbb7300e0cbecd8539375af5d34694f
gustavo2892 <gustavo2892@gmail.com>
2020-11-13 13:27:38 -0300

    Finalized Frontend

--- pages/index.js
+++ pages/index.js
@@ -1,100 +1,134 @@
-import { Form, Select, InputNumber, Switch, Slider, Button } from 'antd'
-
-// Custom DatePicker that uses Day.js instead of Moment.js
-import DatePicker from '../components/DatePicker'
-
-import { SmileFilled } from '@ant-design/icons'
-
-import Link from 'next/link'
+import React, { useState } from 'react';
+import { Form, InputNumber, Button, notification } from 'antd'
+import axios from 'axios';
 
 const FormItem = Form.Item
-const Option = Select.Option
 
 const content = {
-  marginTop: '100px',
+  display: 'flex',
+  height: '100vh',
+  justifyContent: 'center',
+  flexDirection: 'column'
 }
 
 export default function Home() {
+  const [noodleTimeTotal, setNoodleTimeTotal] = useState(1);
+  const [hourglassTimeA, setHourglassTimeA] = useState(1);
+  const [hourglassTimeB, setHourglassTimeB] = useState(1);
+  const [disabledOptions, setDisabledOptions] = useState(false);
+  const [message, setMessage] = useState('Preencha todos os campos');
+  
+  const handleSubmit = () => {
+
+    if (noodleTimeTotal >= hourglassTimeA || noodleTimeTotal >= hourglassTimeB) {
+      const configNotification = {
+        message: 'Verifique os dados',
+        description: 'O tempo das ampulhetas deve ser maior que o tempo de finalização do miojo'
+      }
+
+      notification.warning(configNotification);
+      return;
+    }
+
+    axios({
+      method: 'post',
+      url: '/noodles',
+      data: {
+        noodleTimeTotal,
+        hourglassTimeA,
+        hourglassTimeB
+      }
+    }).then((res) => {
+      setDisabledOptions(true);
+      setMessage(`O tempo necessário para a preparação do miojo é de ${res.data} minutos`);
+    }).catch((error) => {
+      setMessage('Não é possível cozinhar o miojo no tempo exato com as ampulhetas disponíveis');
+    })
+  };
+
+  const handleNewConsultation = () => {
+    setNoodleTimeTotal(1);
+    setHourglassTimeA(1);
+    setHourglassTimeB(1);
+    setMessage('Preencha todos os campos');
+    setDisabledOptions(false);
+  }
+
   return (
     <div style={content}>
+      <div className="text-center mb-3">
+        <h1 className="mb-0 text-disabled">Desafio do Miojo</h1>
+      </div>
       <div className="text-center mb-5">
-        <Link href="#">
-          <a className="logo mr-0">
-            <SmileFilled size={48} strokeWidth={1} />
-          </a>
-        </Link>
-
-        <p className="mb-0 mt-3 text-disabled">Welcome to the world !</p>
+        <h3 className="mb-0 mt-1 text-disabled">{message}</h3>
       </div>
       <div>
         <Form layout="horizontal">
           <FormItem
-            label="Input Number"
+            label="Tempo de finalização do miojo"
             labelCol={{ span: 8 }}
             wrapperCol={{ span: 8 }}
           >
             <InputNumber
               size="large"
               min={1}
-              max={10}
+              value={noodleTimeTotal}
               style={{ width: 100 }}
-              defaultValue={3}
-              name="inputNumber"
+              name="noodleTimeTotal"
+              onChange={setNoodleTimeTotal}
+              disabled={disabledOptions}
             />
+            <span style={{marginLeft: '3px'}}>(min)</span>
           </FormItem>
 
           <FormItem
-            label="Switch"
+            label="Tempo da primeira ampulheta"
             labelCol={{ span: 8 }}
             wrapperCol={{ span: 8 }}
           >
-            <Switch defaultChecked name="switch" />
-          </FormItem>
-
-          <FormItem
-            label="Slider"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 8 }}
-          >
-            <Slider defaultValue={70} />
-          </FormItem>
-
-          <FormItem
-            label="Select"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 8 }}
-          >
-            <Select
+            <InputNumber
               size="large"
-              defaultValue="lucy"
-              style={{ width: 192 }}
-              name="select"
-            >
-              <Option value="jack">jack</Option>
-              <Option value="lucy">lucy</Option>
-              <Option value="disabled" disabled>
-                disabled
-              </Option>
-              <Option value="yiminghe">yiminghe</Option>
-            </Select>
+              min={1}
+              style={{ width: 100 }}
+              value={hourglassTimeA}
+              name="hourglassTimeA"
+              onChange={setHourglassTimeA}
+              disabled={disabledOptions}
+            />
+            <span style={{marginLeft: '3px'}}>(min)</span>
           </FormItem>
 
           <FormItem
-            label="DatePicker"
+            label="Tempo da segunda ampulheta"
             labelCol={{ span: 8 }}
             wrapperCol={{ span: 8 }}
           >
-            <DatePicker name="startDate" />
+            <InputNumber
+              size="large"
+              min={1}
+              style={{ width: 100 }}
+              value={hourglassTimeB}
+              name="inputNumber"
+              onChange={setHourglassTimeB}
+              disabled={disabledOptions}
+            />
+            <span style={{marginLeft: '3px'}}>(min)</span>
           </FormItem>
+
           <FormItem
             style={{ marginTop: 48 }}
             wrapperCol={{ span: 8, offset: 8 }}
           >
-            <Button size="large" type="primary" htmlType="submit">
-              OK
+            <Button 
+              size="large" 
+              type="primary" 
+              onClick={handleSubmit} 
+              disabled={disabledOptions}
+            >
+             Tempo de preparação
             </Button>
-            <Button size="large" style={{ marginLeft: 8 }}>
-              Cancel
+            <Button size="large" style={{ marginLeft: 8 }} onClick={handleNewConsultation} disabled={!disabledOptions}>
+              Fazer nova consulta
             </Button>
           </FormItem>
         </Form>
